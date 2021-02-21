2b7d2724ea8f7bf969549203a36aca4d5973d9c8
dylanw-main <74471780+dylanw-main@users.noreply.github.com>
2021-02-21 17:41:39 -0500

    state machine attempt

--- project2/project2.c
+++ project2/project2.c
@@ -10,14 +10,33 @@
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
 
+int blinkLED(int state);
+
+const uint LED_PIN = 25;
+const int BLINK_LENGTH = 1000;
+
+uint LED_state = 0;
+uint32_t previousMS = 0;
+uint32_t currentMS = 0;
+
 int main() {
-    const uint LED_PIN = 25;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+    while(1){
+        currentMS = to_ms_since_boot(get_absolute_time());
+        blinkLED(LED_state);
     }
+}
+int blinkLED(int state) {
+    if (currentMS - previousMS >= BLINK_LENGTH) {
+        previousMS = currentMS;
+        if (state == 0) {
+            gpio_put(LED_PIN, 1);
+            state = 1;
+        } else if (state == 1) {
+            gpio_put(LED_PIN, 0);
+            state = 0;
+        }
+    }
+    return LED_state = state;
 }
